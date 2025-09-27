602620d25ffc46ca919c04fddb61cd9a67f9e3cf
hamzakhanbtm <12345678+hamza05mrd@users.noreply.github.com>
2025-09-27 21:37:01 +0500

    jwttoken

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -13,6 +13,8 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRY * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: 'None',
+      secure: true,
     })
     .json({
       success: true,
